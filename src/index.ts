7b919ae1a54bdfb7fb89cb89ef8c2851e6f4e2ac
sxend <arimitsu.k+github@gmail.com>
2020-10-12 21:30:40 +0900

    fix tab witdh

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Model } from "./model";
 
 export const App = {
-  model: new Model(),
+    model: new Model(),
 };
--- src/model.ts
+++ src/model.ts
@@ -1,10 +1,10 @@
 export class Model {
-  private collection: any = {};
-  constructor() {}
-  get(key: string): any {
-    return this.collection[key];
-  }
-  set(key: string, value: any): void {
-    this.collection[key] = value;
-  }
+    private collection: any = {};
+    constructor() {}
+    get(key: string): any {
+        return this.collection[key];
+    }
+    set(key: string, value: any): void {
+        this.collection[key] = value;
+    }
 }
